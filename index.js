1f767ee35e07c7167feef7c37b6d12afeb0c3636
vladmorari <morariv67@gmail.com>
2021-03-22 01:41:43 +0200

    *

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const server = require("./api/server");
-require("dotenv").config();
 const port =process.env.PORT|| 8000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
